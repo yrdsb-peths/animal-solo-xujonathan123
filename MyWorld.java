29c9af3d4a598b3cbd47f355318604235ade93ce
xujonathan123 <xujonathan123@gmail.com>
2025-11-20 16:25:36 -0500

    add increaseScore function

--- MyWorld.java
+++ MyWorld.java
@@ -1,6 +1,8 @@
 import greenfoot.*;
 
 public class MyWorld extends World {
+    public int score;
+    Label scoreLabel;
     public MyWorld() {
         super(600, 400, 1);
         //create elephant class
@@ -8,11 +10,18 @@ public class MyWorld extends World {
         addObject(elephant, 300, 200);
         
         //add label
-        Label scoreLabel = new Label(0, 20);
-        addObject(scoreLabel, 0, 0);
+        scoreLabel = new Label(0, 20);
+        addObject(scoreLabel, 50, 50);
         
         //spawn initial apple
         createApple();
+    }
+    /*
+     * increment score
+     */
+    public void increaseScore(){
+        score++;
+        scoreLabel.setValue(score);
     }
     
     /*
