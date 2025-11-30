3aa6574d70e38e978c2dea4d993c68fd6422d77e
xujonathan123 <xujonathan123@gmail.com>
2025-11-30 12:50:19 -0500

    add animation to elephant

--- Elephant.java
+++ Elephant.java
@@ -9,13 +9,21 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Elephant extends Actor
 {
     GreenfootSound elephantSound = new GreenfootSound("elephantsound.mp3");
-    GreenfootImage idle = new GreenfootImage("images/elephant_idle/idle0.png");
+    GreenfootImage[] idle = new GreenfootImage[8];
     
     /*
      * Constructor
        */
     public Elephant(){
-        setImage(idle);
+        for(int i = 0; i<8; i++){
+            idle[i] = new GreenfootImage("images/elephant_idle/idle" + i +".png");
+        }
+        setImage(idle[0]);
+    }
+    int imageIndex = 0;
+    public void animateElephant(){
+        setImage(idle[imageIndex]);
+        imageIndex = (imageIndex + 1) % idle.length;
     }
     
     /**
@@ -35,6 +43,8 @@ public class Elephant extends Actor
         }
         //remove apple when touching elephant
         eat();
+        
+        animateElephant();
     }
     /*
      * eat function
