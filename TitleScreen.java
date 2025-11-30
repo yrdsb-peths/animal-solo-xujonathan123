1787f2534ad3e50ee1b07b30f1db2adaa7c653ca
xujonathan123 <xujonathan123@gmail.com>
2025-11-30 13:17:06 -0500

    add more objects visually onto title screen

--- TitleScreen.java
+++ TitleScreen.java
@@ -17,13 +17,36 @@ public class TitleScreen extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
-        addObject(titleLabel, getWidth()/2, getHeight()/2);
+        addObject(titleLabel, getWidth()/2, 150);
+        prepare();
     }
-    
+
     public void act(){
         if(Greenfoot.isKeyDown("space")){
             MyWorld gameWorld = new MyWorld();
             Greenfoot.setWorld(gameWorld);
         }
+    }
+    
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        Elephant elephant = new Elephant();
+        addObject(elephant,545,77);
+        elephant.setLocation(438,111) ;
+        elephant.setLocation(535,50);
+        elephant.setLocation(538,61);
+        elephant.setLocation(539,63);
+        Label label = new Label("use arrow keys to move", 50);
+        addObject(label,253,233);
+        label.setLocation(304,225);
+        Label label2 = new Label("press <space> to begin", 50);
+        addObject(label2,253,310);
+        label2.setLocation(317,307);
+        label2.setLocation(302,305);
+        label.setLocation(311,232);
     }
 }
